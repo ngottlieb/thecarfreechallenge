af3426a2a1bf6d036c9859b1d84054e6af908595
Nick Gottlieb <ngottlieb@gmail.com>
2017-12-27 11:13:21 -0800

    add authenticated user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+  authenticated :user do
+    root to: 'home#index', as: :authenticated_root
+  end
   root to: 'visitors#index'
 end
