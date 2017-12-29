e65168c9cd5ca3502d87a0167ef06104c1f11e0a
Nick Gottlieb <ngottlieb@gmail.com>
2017-12-28 19:28:30 -0800

    set up layouts for sign in, forgot password, register

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,12 @@ module CarFreeAdventureChallenge
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.to_prepare do
+      Devise::SessionsController.layout 'devise'
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::PasswordsController.layout "devise"
+      Devise::UnlocksController.layout "devise"
+    end
   end
 end
