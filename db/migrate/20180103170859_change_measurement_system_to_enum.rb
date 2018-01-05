840de0a38ebe54ca98c7c479a87faa619ee3ede8
Nick Gottlieb <ngottlieb@gmail.com>
2018-01-05 10:59:07 -0800

    fix measurement system migration

--- db/migrate/20180103170859_change_measurement_system_to_enum.rb
+++ db/migrate/20180103170859_change_measurement_system_to_enum.rb
@@ -1,11 +1,11 @@
 class ChangeMeasurementSystemToEnum < ActiveRecord::Migration[5.1]
   def change
     rename_column :users, :measurement_system, :old_measurement_system
-    add_column :users, :measurement_system, :integer, default: User.measurement_systems[:imperial]
+    add_column :users, :measurement_system, :integer, default: User.measurement_systems[:imperial_system]
 
     # transfer data from old column
     User.all.each do |u|
-      u.measurement_system = u.old_measurement_system
+      u.measurement_system = u.old_measurement_system + '_system'
       u.save!
     end
 
