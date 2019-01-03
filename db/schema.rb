56a11611fb2ac331d86d69850e39349740ae112a
Nick Gottlieb <ngottlieb@gmail.com>
2019-01-02 18:32:39 -0800

    persist strava access token in db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108031644) do
+ActiveRecord::Schema.define(version: 20190103021522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180108031644) do
     t.string "name"
     t.integer "measurement_system", default: 0
     t.boolean "import_in_progress", default: false
+    t.string "strava_access_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
