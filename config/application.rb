cc89ca1f5682d2573f19f3d2766bb8fc11c64e39
Nick Gottlieb <ngottlieb@gmail.com>
2022-07-08 16:07:52 -0700

    first pass at upgrading to rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,13 +18,16 @@ module CarFreeAdventureChallenge
         controller_specs: false,
         request_specs: false
     end
-
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
     
     config.to_prepare do
       Devise::SessionsController.layout 'devise'
