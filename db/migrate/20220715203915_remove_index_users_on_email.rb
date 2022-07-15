ecc41a2732b35929432b5e63e364522ad070332c
Nick Gottlieb <ngottlieb@gmail.com>
2022-07-15 13:40:54 -0700

    remove user email index

---
+++ db/migrate/20220715203915_remove_index_users_on_email.rb
@@ -0,0 +1,5 @@
+class RemoveIndexUsersOnEmail < ActiveRecord::Migration[6.1]
+  def change
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_182414) do
+ActiveRecord::Schema.define(version: 2022_07_15_203915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,7 +106,6 @@ ActiveRecord::Schema.define(version: 2022_07_12_182414) do
     t.boolean "import_in_progress", default: false
     t.string "strava_access_token"
     t.boolean "admin", default: false
-    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
