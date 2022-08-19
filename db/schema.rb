47089ee5bc4fab04b1a103bd71a687304c8013bc
Nick Gottlieb <ngottlieb@gmail.com>
2022-08-19 16:53:11 -0700

    add mailer for user notifications of milestone achievements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_18_185915) do
+ActiveRecord::Schema.define(version: 2022_08_19_232350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2022_08_18_185915) do
     t.string "strava_refresh_token"
     t.boolean "opt_out_of_leaderboard", default: false
     t.boolean "opt_out_of_barueat_emails", default: false
+    t.boolean "opt_out_of_milestone_notifications", default: false
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
