f517236c8a1a4a2cc278ef57e16336438cf79204
Nick Gottlieb <ngottlieb@gmail.com>
2022-08-26 12:45:17 -0700

    use tagline for user milestone notification email subject

--- app/mailers/user_notifications_mailer.rb
+++ app/mailers/user_notifications_mailer.rb
@@ -6,8 +6,15 @@ class UserNotificationsMailer < ApplicationMailer
   def notify_of_milestone_achievement
     @user = params[:user]
     @milestone = params[:milestone]
+
+    if @milestone.tagline.present?
+      subject = "Congrats! You earned the #{@milestone.tagline} badge!"
+    else
+      subject = "Congrats! You've #{milestone_achievement_verb(@milestone)} #{friendly_display_of_threshold(@milestone, @user)}"
+    end
+    
     mail(
-      subject: "Congrats! You've #{milestone_achievement_verb(@milestone)} #{friendly_display_of_threshold(@milestone, @user)}",
+      subject: subject,
       to: @user.email
     )
   end
